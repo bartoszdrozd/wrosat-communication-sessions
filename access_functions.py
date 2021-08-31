9525e3c8886a917f27441d2085473b24de84c7e6
Bartosz Drozd <bartosz.drozd94@gmail.com>
2021-08-31 21:14:15 +0200

    Updated figures + sorted data

--- access_functions.py
+++ access_functions.py
@@ -4,45 +4,47 @@ import calendar
 import matplotlib.pyplot as plt
 
 
-def access_times(df, itemIndex, durationList, file_name):
+def access_times(df, item_index, duration_list, file_name):
     # one fuction to show data about accesses in one specific csv file
     # improve that loop so it automatically sets limit value
     for i in range(1, len(df)):
         if df['Duration (sec)'][i] < 50:
-            itemIndex[0] += 1
+            item_index[0] += 1
         elif df['Duration (sec)'][i] < 100:
-            itemIndex[1] += 1
+            item_index[1] += 1
         elif df['Duration (sec)'][i] < 150:
-            itemIndex[2] += 1
+            item_index[2] += 1
         elif df['Duration (sec)'][i] < 200:
-            itemIndex[3] += 1
+            item_index[3] += 1
         elif df['Duration (sec)'][i] < 250:
-            itemIndex[4] += 1
+            item_index[4] += 1
         elif df['Duration (sec)'][i] < 300:
-            itemIndex[5] += 1
+            item_index[5] += 1
         elif df['Duration (sec)'][i] < 350:
-            itemIndex[6] += 1
+            item_index[6] += 1
         elif df['Duration (sec)'][i] < 400:
-            itemIndex[7] += 1
+            item_index[7] += 1
         elif df['Duration (sec)'][i] < 450:
-            itemIndex[8] += 1
+            item_index[8] += 1
         elif df['Duration (sec)'][i] < 500:
-            itemIndex[9] += 1
+            item_index[9] += 1
         elif df['Duration (sec)'][i] < 550:
-            itemIndex[10] += 1
+            item_index[10] += 1
 
     # with open(f'{file_name}.txt', 'w+') as file:
     # 	#file.write(f"Max access time: {max_value}\n")
     # 	file.write(f"Median of access times: {df['Duration (sec)'].median()}\n")
     # 	file.write(f"Mean access time: {df['Duration (sec)'].mean()}")
 
-    y_pos = np.arange(len(itemIndex))
-    plt.bar(y_pos, itemIndex, align='center', alpha=0.5)
-    plt.xticks(y_pos, durationList)
-    plt.ylabel('No of accesses')
-    plt.xlabel('Sec')
-    plt.title('Access Duration')
+    y_pos = np.arange(len(item_index))
+    plt.figure(0)
+    plt.bar(y_pos, item_index, align='center', alpha=0.5)
+    plt.xticks(y_pos, duration_list)
+    plt.ylabel('Liczba dostępów')
+    plt.xlabel('Czas[s]')
+    plt.title(f'Długość sesji komunikacyjnych dla {file_name}')
     plt.savefig(f'figures/{file_name}.png')
+    plt.clf()
 
 
 def access_calculation(df, file_name):
@@ -59,8 +61,10 @@ def access_calculation(df, file_name):
             window_list.append(window_time)
             date_list.append(df['Stop Time (UTCG)'][i])
 
-    plt.ylabel('Time in between accesses [seconds]')
-    plt.xlabel('Time')
-    plt.title(f'Length of radio silence for {file_name}')
+    plt.figure()
+    plt.ylabel(f'Długość przerwy radiowej dla {file_name}[s]')
+    plt.xlabel('Czas od rozpoczęcia misji')
+    plt.title(f'Długość przerw radiowych dla {file_name}')
     plt.scatter(date_list, window_list)
     plt.savefig(f'figures/length_of_silence_for_{file_name}.png')
+    plt.clf()
--- main.py
+++ main.py
@@ -9,22 +9,24 @@ document = Document()
 pathName = os.getcwd()
 csv_location = pathName + '/csvfiles'
 fileNames = os.listdir(csv_location)
+fileNames.sort()
 
 csv_counter = 0
 for fileName in fileNames:
 	file_to_search = 'csvfiles/' + fileName
 	df = pd.read_csv(file_to_search, index_col='Access')
-	file_name = re.search('[A-Z]{3}\d{0,3}', file_to_search).group(0)
+	file_name = re.search("[A-Z]{3}\\d{0,3}", file_to_search).group(0)
 	column = df['Duration (sec)']
 	max_value = int(column.max())
-	durationList = list(range(50, max_value+50, 50))
-	itemIndex = [0] * len(durationList)
+	duration_list = list(range(50, max_value+50, 50))
+	item_index = [0] * len(duration_list)
 	if csv_counter < len(fileNames):
-		access_times(df, itemIndex, durationList, file_name)
+		access_times(df, item_index, duration_list, file_name)
 		analysis_text = f"""
-						Maksymalna długość sesji komunikacyjnej: {df['Duration (sec)'].max()}\n \
-						Mediana czasów sesji komunikacyjnych: {df['Duration (sec)'].median()}\n \
-						Średnia długość sesji komunikacyjnej: {df['Duration (sec)'].mean()}"""
+		Dane dla orbity: {file_name}
+		Maksymalna długość sesji komunikacyjnej: {df['Duration (sec)'].max()}\n \
+		Mediana czasów sesji komunikacyjnych: {df['Duration (sec)'].median()}\n \
+		Średnia długość sesji komunikacyjnej: {df['Duration (sec)'].mean()}"""
 		document.add_paragraph(analysis_text)
 		document.add_picture(f'figures/{file_name}.png')
 		access_calculation(df, file_name)
